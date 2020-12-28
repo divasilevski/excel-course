c81b411a1fce9c1638f3e60863727c0db933a39c
Dmitry Vasilevsky <dmitry.vasilevsky@immelman.ru>
2020-12-28 10:34:02 +0300

    Ивенты

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -3,9 +3,24 @@ import { ExcelComponent } from '../../core/ExcelComponent'
 export class Formula extends ExcelComponent {
   static className = 'excel__formula'
 
+  constructor($root) {
+    super($root, {
+      name: 'Formula',
+      listeners: ['input', 'click'],
+    })
+  }
+
   toHTML() {
-    return `
+    return html`
     <div class="info">fx</div>
     <div class="input" contenteditable spellcheck="false"></div>`
+  }
+
+  onInput(event) {
+    console.log('formula', event.target.textContent.trim())
+  }
+
+  onClick(event) {
+    console.log('click')
   }
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,8 +1,16 @@
 import { DomListener } from './DomListener'
 
 export class ExcelComponent extends DomListener {
+  constructor($root, options = {}) {
+    super($root, options.listeners)
+    this.name = options.name || ''
+  }
   // Возвращает шаблон компонента
   toHTML() {
     return '<h1>Component</h1>'
+  }
+
+  init() {
+    this.initDOMListeners()
   }
 }
