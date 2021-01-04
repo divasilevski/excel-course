058826d85782674e4b56b5b28f0ae32d0dbe1b0c
Dmitry Vasilevsky <dmitry.vasilevsky@immelman.ru>
2021-01-04 22:19:06 +0300

    Добавлен имиттер

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -13,10 +13,11 @@ import { $ } from '../../core/dom'
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
-  constructor($root) {
+  constructor($root, options) {
     super($root, {
       name: 'Table',
-      listeners: ['mousedown', 'keydown'],
+      listeners: ['mousedown', 'keydown', 'input'],
+      ...options,
     })
   }
 
@@ -31,8 +32,15 @@ export class Table extends ExcelComponent {
   init() {
     super.init()
 
-    const cell = this.$root.find('[data-id="0:0"]')
-    this.selection.select(cell)
+    const $cell = this.$root.find('[data-id="0:0"]')
+    this.selectCell($cell)
+
+    this.$on('formula:input', (text) => {
+      this.selection.current.text(text)
+    })
+    this.$on('formula:done', () => {
+      this.selection.current.focus()
+    })
   }
 
   onMousedown(event) {
@@ -59,11 +67,20 @@ export class Table extends ExcelComponent {
       event.preventDefault()
       const id = this.selection.current.id(true)
       const $next = this.$root.find(nextSelector(key, id))
-      this.selection.select($next)
+      this.selectCell($next)
     }
   }
 
+  onInput(event) {
+    this.$emit('table:input', $(event.target))
+  }
+
   // Custom Methods
+  selectCell($cell) {
+    this.selection.select($cell)
+    this.$emit('table:select', $cell)
+  }
+
   selectHandler(event) {
     const $target = $(event.target)
     if (event.shiftKey) {
